fdb2e1fde482a21e6602e1568f1adbf5d08b80d7
spratth <stavroulaprt@gmail.com>
2025-05-21 22:41:11 +0300

    first components

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+import Layout from "./components/Layout.tsx";
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
+import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+
+
+function App() {
+
+    return (
+        <>
+            <Layout>
+                {/*<ClassComponent/>*/}
+                {/*<FunctionalComponent/>*/}
+                {/*<ArrowFunctionalComponent/>*/}
+                {/*<ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component with Props!"/>*/}
+                {/*<ArrowFunctionalComponentWithPropsType*/}
+                {/*  title="Is a Arrow Functional Component with Props!"*/}
+                {/*  description="this is a description"*/}
+                {/*/>*/}
+
+                {/*<ClassComponentWithState/>*/}
+                <FunctionalComponentWithState/>
+
+
+            </Layout>
+        </>
+    )
+}
+
+export default App
